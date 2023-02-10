ade92fec9cabe503ba9cc27b9ef7ad8f6a3dacc7
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-10 13:39:24 +0300

    exercise 33 had been accomplished

---
+++ exercise_33/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] Array = {6,7,19,345,3};
+Console.WriteLine("Enter a digit you want to check: ");
+int n = Int32.Parse(Console.ReadLine());
+
+int ForCompare=0;
+foreach (int element in Array)
+{
+    if (element == n)
+    {
+        Console.WriteLine("Your number is contained in array");
+        ForCompare = n;
+        break;
+    }
+}
+if (ForCompare != n) Console.WriteLine("Your number is NOT contained in array");
