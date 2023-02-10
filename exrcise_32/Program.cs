a1e6d75ae996c2051f678e089630a873d52c1a06
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-10 13:04:21 +0300

    exercise 32 had been accomplished

---
+++ exrcise_32/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] Array = new int[new Random().Next(3,7)];
+int[] NewArray = new int[Array.Length];
+
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(-11,11);
+    Console.Write($"{Array[i]} ");
+    NewArray[i] = -Array[i];
+    Console.WriteLine($"{NewArray[i]} ");
+}
