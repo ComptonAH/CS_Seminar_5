78301672dd6a7847c0bd80ee389e4aa7563146dd
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-10 13:48:14 +0300

    exercise 35 had been accomplished

---
+++ exercise_35/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] Array = new int[123];
+
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(180);
+}
+int count = 0;
+foreach (int element in Array)
+{
+    if (element > 9 && element < 100)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
