8c68c44ed8912be9e4174dd248d4e03aed512c3d
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-10 12:41:09 +0300

    exercise 31 had been accomplished

---
+++ exercise_31/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] Array = new int[12];
+
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(-10,10);
+    Console.Write($"{Array[i]} ");
+}
+
+int PosResult = 0;
+int NegatResult = 0;
+
+foreach (int element in Array)
+{
+    if (element>0) PosResult = PosResult + element;
+    else NegatResult = NegatResult - element;
+}
+
+// Для наглядности добавлены выводы значений в консоль
+Console.WriteLine();
+Console.WriteLine(PosResult);
+Console.WriteLine(~NegatResult);
